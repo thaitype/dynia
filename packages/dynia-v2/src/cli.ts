cf6e6448c97e3f6d4f695721dbe4b1c2fea08117
Thada Wangthammang <thada.wth@gmail.com>
2025-08-14 23:54:03 +0700

    feat: Initialize basic-v2 example with essential configuration files and CLI entry point

---
+++ packages/dynia-v2/src/cli.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+
+import { ConsoleLogger } from '@thaitype/core-utils';
+
+import { cliEntryPoint } from './cli-interfaces/entrypoint.js';
+import { handlerError } from './internal/error.js';
+import { version } from './version.js';
+
+// Set up the CLI entry point
+// This is the main entry point for the CLI application.
+cliEntryPoint(process.argv, {
+  version,
+  scriptName: 'dynia',
+}).catch(err => {
+  handlerError(err, new ConsoleLogger('silent'), 99);
+});
